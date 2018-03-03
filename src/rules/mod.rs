09dd81a2b1539edfec3b14dbb5621ebea83d8740
Roman Kolacz <roman@opengamma.com>
2018-03-03 20:14:36 +0000

    Rename game to rules

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -1,11 +1,10 @@
-//! Chess game module, primarily revolving around the rules.
+//! Chess rules module.
 //!
 //! This module contains rules around the movement of pieces and
 //! general victory/defeat conditions.
 //!
 //! This includes:
 //! * Which pieces can move where
-//! * Only the player whose turn it is can move a piece
 //! * Castling
 //! * En passant capturing
 //! * Promotion
@@ -20,4 +19,3 @@ mod semilegal_moves;
 #[cfg(test)]
 mod test;
 
-// TODO: Should move generation be split out of this?
